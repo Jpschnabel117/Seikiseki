66556e588cdbcf521ad406219ba6fe6ac9aa5da9
Jpschnabel177 <jpschnabel96@gmail.com>
2023-03-03 22:45:48 -0500

    beeboop

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './css/App.css'
 
-function App() {
+function App(props) {
   const [count, setCount] = useState(0)
 
   return (
