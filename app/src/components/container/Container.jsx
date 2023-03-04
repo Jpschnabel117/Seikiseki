2f8ad2c9a6591c8f94382f51a866562e1aa7691b
Jpschnabel177 <jpschnabel96@gmail.com>
2023-03-04 17:57:05 -0500

    working on d3 svg overlay alignment

--- app/src/components/container/Container.jsx
+++ app/src/components/container/Container.jsx
@@ -1,11 +1,27 @@
+import * as d3 from "d3";
+import React, { useEffect } from "react";
 import worldMap from "../../assets/wrld-bp-1-svg.svg";
 
 function Container() {
+  useEffect(() => {
+    const svg = d3.select("svg");
+    svg
+      .append("circle")
+      .attr("cx", 100)
+      .attr("cy", 120)
+      .attr("r", 100)
+      .attr("fill", "red")
+      .transition()
+      .duration(1000)
+      .attr("r", 1000)
+
+  }, []);
+
   return (
     <div className="mapScreen">
-      <p>assdasddasfsdf</p>
       <img src={worldMap} className="worldMap" alt="mapsvg" />
-      
+      <svg id="sites" className="overlay">      
+      </svg>
     </div>
   );
 }
