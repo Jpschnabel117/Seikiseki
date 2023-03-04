b975844230e1dcf2ab94f2127c723f38ce2fe426
mikey <michael_almeida@cymantix.com>
2023-03-04 11:30:48 -0500

    cors options for vite dev server.. need to move to env

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+
+const corsOptions = {
+	origin : 'http://localhost:5173/',
+	 optionsSuccessStatus: 200 
+	}
+
+
 app.use((req, res, next) => {
   console.log('Time: ', Date.now());
   next();
 });
 
+app.use(cors(corsOptions));
 app.get('./', (req, res) => {
 	console.log('Recieved response');
 	res.send('Hello world!!');
