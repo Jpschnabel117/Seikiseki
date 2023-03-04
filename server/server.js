c7060c78f7a6e62e36c97dbdd969f8f00d324088
mikey <michael_almeida@cymantix.com>
2023-03-04 14:24:58 -0500

    cors for dev server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors')
 
 const corsOptions = {
-	origin : 'http://localhost:5173/',
+	origin : 'http://localhost:5173',
+	methods:["POST", "GET"],
 	 optionsSuccessStatus: 200 
 	}
 
@@ -12,9 +13,9 @@ app.use((req, res, next) => {
   console.log('Time: ', Date.now());
   next();
 });
-app.use(cors)
+app.options('/',cors(corsOptions))
 // app.use(cors(corsOptions));
-app.get('/', cors(corsOptions), (req, res) => {
+app.get('/', cors(corsOptions),(req, res, next) => {
 	console.log('Recieved response');
 	res.send('Hello world!!');
 });
