8e88238b5e7ed744c6940842460476355156115b
Jpschnabel177 <jpschnabel96@gmail.com>
2023-03-05 16:23:23 -0500

    graticules added

--- app/src/components/container/Worldmapmarks.jsx
+++ app/src/components/container/Worldmapmarks.jsx
@@ -7,10 +7,12 @@ function Worldmapmarks(props) {
 
   const projection = d3.geoEquirectangular(); //change projections here
   const path = d3.geoPath(projection);
+  const graticule = d3.geoGraticule()
 
   return (
     <g className="worldMapMarks">
       <path className="sphere" d={path({ type: "Sphere" })} />
+      <path className="graticules" d={path(graticule())} />
       {data.features.map((feature) => (
         <path className="wMapFeature" d={path(feature)} />
       ))}
