a4ada9d3f6c99bd01169adfedde50e07603848bc
mikey <michael_almeida@cymantix.com>
2023-03-05 13:07:08 -0500

    SQL Tables setup, Launch table populated from api

--- test.session.sql
+++ test.session.sql
@@ -2,35 +2,34 @@
 CREATE TABLE Users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
+    pass VARCHAR(255) NOT NULL,
     bio TEXT,
     country VARCHAR(2)
 );
 -- @block
 CREATE TABLE Launches(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    location_name VARCHAR(255) NOT NULL UNIQUE
+    location_name VARCHAR(255) NOT NULL UNIQUE,
+    country VARCHAR(2),
+    longitude INT,
+    latitude INT,
+    utc_offset INT(1)
 );
 -- @block
 CREATE TABLE Watching (
     user_id INT,
     launch_id INT,
-    PRIMARY_KEY (user_id, launch_id),
-    FOREIGN_KEY (user_id) REFERENCES Users(id),
-    FOREIGN_KEY (launch_id) REFERENCES Launches(id)
-) -- @block
+    PRIMARY KEY (user_id, launch_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (launch_id) REFERENCES Launches(id)
+);
+-- @block
+DROP TABLE Watching;
+DROP TABLE Launches;
+-- @block 
+INSERT INTO Users(email, bio, country)
+VALUES ("test", "testing bio", "US");
+-- @block
 SELECT email,
     id
-FROM Users
-WHERE country = 'US'
-ORDER BY id ASC
-LIMIT 1;
--- @block
-CREATE INDEX email_index ON Users(email);
--- @block
-CREATE TABLE Rooms(
-    id INT AUTO_INCREMENT,
-    street VARCHAR(255),
-.owner__iud INT NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (OWN)
-)
+from Users;
