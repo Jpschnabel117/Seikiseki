d5f02d0b0c20b69705988501fbdfddec45c9b8e0
Jpschnabel177 <jpschnabel96@gmail.com>
2023-03-05 12:51:41 -0500

    "added mapJSON, preparing for SVG conversion"

--- app/src/components/container/Container.jsx
+++ app/src/components/container/Container.jsx
@@ -1,26 +1,22 @@
 import * as d3 from "d3";
-import React, { useEffect } from "react";
+import React, { useEffect} from "react";
 import worldMap from "../../assets/wrld-bp-1-svg.svg";
+import Launchsite from "./Launchsite";
 
 function Container() {
-  useEffect(() => {
-    const svg = d3.select("svg");
-    svg
-      .append("circle")
-      .attr("cx", 100)
-      .attr("cy", 120)
-      .attr("r", 100)
-      .attr("fill", "red")
-      .transition()
-      .duration(1000)
-      .attr("r", 1000)
-
-  }, []);
+    let width;
+    let height;
+    // if(!data){
+    //     return <pre>loading...</pre>
+    // }
 
   return (
     <div className="mapScreen">
-      <img src={worldMap} className="worldMap" alt="mapsvg" />
-      <svg id="sites" className="overlay">      
+      {/* <img src={worldMap} className="worldMap" alt="mapsvg" /> */}
+      <svg id="sites" className="overlay">
+        <g>
+          <Launchsite />
+        </g>
       </svg>
     </div>
   );
---
+++ app/src/components/container/Launchsite.jsx
@@ -0,0 +1,14 @@
+import * as d3 from "d3";
+import React, { useEffect } from "react";
+import worldMap from "../../assets/wrld-bp-1-svg.svg";
+
+function Launchsite(props) {
+
+  return (
+    <>
+      <circle cx="100" cy="100" r="10" fill="red" />
+    </>
+  );
+}
+
+export default Launchsite;
