efc81cf9fa6c9a074c941abee5d67e8844c7ab27
mikey <mjalmeida172@gmail.com>
2023-03-06 17:52:22 -0500

    [no jira]

--- app/src/redux/reducers/index.js
+++ app/src/redux/reducers/index.js
@@ -1,5 +1,7 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 import container from './container.js';
+const reducers = combineReducers(
+	{ container }
 
-const reducers = combineReducers({container})
+)
 export default reducers;
