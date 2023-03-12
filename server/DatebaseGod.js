a84f040b33c9bb8f8391c6b7d1ad9cb396c61ddd
mikey <mjalmeida172@gmail.com>
2023-03-11 19:52:16 -0500

    GETTING LOCKED OUT WHAT THE FAWK

--- server/DatebaseGod.js
+++ server/DatebaseGod.js
@@ -5,60 +5,82 @@
 // need to run this twice, once on page 1, and once on page 2
 require('dotenv').config(); // Load environment variables
 const request = require('request');
+const mysql = require('mysql2');
 
-let last_page;
+const connection = mysql.createConnection({
+  database: 'seikiseki',
+  user: process.env.user,
+  password: process.env.password,
+  host: process.env.host,
+  port: process.env.port,
+});
 
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
-async function populateLaunchTableData(dataarray = [], page = 1) {
-  const options = {
+async function populateLaunchTableData() {
+  const options =(page) => ({
     method: 'GET',
     url: `https://fdo.rocketlaunch.live/json/launches?after_date=1963-01-01&page=${page}`,
     headers: {
       Authorization: `Bearer ${process.env.api_key}`,
     },
-  };
-  const data = {};
-  try {
-    request(options, async (err, results) => {
-      try {
-        data = (results.body);
-      } catch (error) {
-        console.error('Error parsing JSON:', err);
-        console.error('Response body:', results.body);
-        throw new Error('Error parsing JSON');
-      }
+  });
+  const dataarray = [];
+  const last_page = 100;
+
+
+  for (let page = 1; page <= 3; page++) {
+    setInterval(() => {
+      request(options(page), async (err, results) => {
+        try {
+          console.log(page);
+          data= JSON.parse(results.body);
+          for (const launch of data['result']) {
+            console.log(launch.id);
+            const obj = {
+              'id': launch.id,
+              'sort_date': launch['sort_date'],
+              'rocket_name': launch['name'],
+              'provider': launch['provider.name'],
+              'vehicle': launch['vehicle.name'],
+              'date_str': launch['date_str'],
+              'quicktext': launch['quicktext'],
+              'result': launch['result'],
+            };
+            dataarray.push(obj);
+          }
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+          console.error('Response body:', results.body);
+          throw new Error('Error parsing JSON');
+        }
+      }, (10000));
     });
+  }
 
-    if (!last_page) {
-      last_page = data.last_page;
-    }
 
-    if (page > last_page) {
-      return {
-        message: `inserted ${page} pages of data`,
-        data: putData(dataarray),
-      };
-    }
+  for (i in dataarray) {
+    const {id, sort_date, rocket_name,
+      provider, vehicle, date_str, quicktext, result} = i;
+    const sql = `INSERT INTO LaunchData(
+      id, sort_date, rocket_name, 
+      launch_site, prov, vehicle, 
+      date_str, quicktext, result) 
+      VALUES('${id}','${sort_date}','${rocket_name}',
+        '${provider}','${vehicle}', '${date_str}',
+         '${quicktext}', '${result}')`;
 
-    for (const launch of await data.result) {
-      const obj = {
-        'id': launch.id,
-        'sort_date': launch['sort_date'],
-        'rocket_name': launch['name'],
-        'provider': launch['provider.name'],
-        'vehicle': launch['vehicle.name'],
-        'date_str': launch['date_str'],
-        'quicktext': launch['quicktext'],
-        'result': launch['result'],
-      };
 
-      dataarray.push(obj);
-      page++;
-
-      await populateLaunchTableData(dataarray, page);
-    }
-  } catch (error) {
-    console.error(error);
+    connection.query(sql, function(err, result) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
+      console.log('1 record inserted');
+    });
   }
 };
 
@@ -102,6 +124,5 @@ populateLaunches = async () => {
     });
   }
 };
-
 
 populateLaunchTableData();
